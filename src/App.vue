cc7209fd6eaf3c4f635c5be86bff4504da4da982
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-12-14 23:45:59 +0100

    Primera parte resuelta

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup>
-
 import { ref, computed } from 'vue';
 
-let textOnDisplay = "";
+let textOnDisplay = ref("");
 let showOnDisplay = ref(0);
 
 const showDisplay = computed({
@@ -14,17 +13,17 @@ const showDisplay = computed({
 const addToDisplay = function (element) {
   if (element == "c") {
     showOnDisplay.value = 0;
-    textOnDisplay = "";
+    textOnDisplay.value = "";
     return
   }
   if (element == "=") {
-    showOnDisplay.value = eval(showOnDisplay);
-    textOnDisplay = textOnDisplay.value;
+    showOnDisplay.value = eval(showOnDisplay.value);
+    textOnDisplay.value = showOnDisplay.value;
     return
   }
 
-  textOnDisplay += element;
-  showOnDisplay.value = showOnDisplay;
+  textOnDisplay.value += element;
+  showOnDisplay.value = textOnDisplay.value;
 }
 
 </script>
@@ -34,6 +33,14 @@ const addToDisplay = function (element) {
     <p id="display">{{ showDisplay }}</p>
     <button id="oneKey" @click="addToDisplay(1)">1</button>
     <button id="twoKey" @click="addToDisplay(2)">2</button>
+    <button id="threeKey" @click="addToDisplay(3)">3</button>
+    <button id="fourKey" @click="addToDisplay(4)">4</button>
+    <button id="fiveKey" @click="addToDisplay(5)">5</button>
+    <button id="sixKey" @click="addToDisplay(6)">6</button>
+    <button id="sevenKey" @click="addToDisplay(7)">7</button>
+    <button id="heightKey" @click="addToDisplay(8)">8</button>
+    <button id="nineKey" @click="addToDisplay(9)">9</button>
+    <button id="zeroKey" @click="addToDisplay(0)">0</button>
     <button id="additionKey" @click="addToDisplay(`+`)">+</button>
     <button id="subtractionKey" @click="addToDisplay(`-`)">-</button>
     <button id="multiplicationKey" @click="addToDisplay(`*`)">*</button>
@@ -56,6 +63,7 @@ button {
   border-radius: 5px;
   border: 1px solid black;
   font-size: 24px;
+  cursor: pointer;
 }
 
 .resetButton {
